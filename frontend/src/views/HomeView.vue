26e6ee961c049b3544e7666f999f78e726c8573e
Rick Flagg <richard.lee.flagg@outlook.com>
2024-03-02 19:56:28 -0500

    Adding leaflet spike

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,46 @@
 <script setup>
 
+import { onMounted, ref } from 'vue'
+import L from 'leaflet'
+import "leaflet/dist/leaflet.css"
+
+const map = ref(null)
+
+function formatMap(){
+  console.log(map);
+
+  let mapref = L.map(map.value).setView([38.763250, -77.298793], 15); 
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mapref);
+
+}
+
+
+onMounted(() =>{
+  formatMap();
+})
+
 </script>
 
 <template>
   <main>
+
+    
+
     Hello
+
+    <div class="map-wrapper" ref="map"></div>
+
   </main>
 </template>
+
+
+<style>
+.map-wrapper {
+  width: 100%;
+  height: 400px;
+}
+</style>
