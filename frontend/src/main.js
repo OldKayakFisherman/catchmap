54eebd3a21a221bafa579692a2fbd62df607ed45
Rick Flagg <richard.lee.flagg@outlook.com>
2024-03-04 07:13:38 -0500

    Working out leaflet integration

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 import './assets/main.css'
 import 'leaflet/dist/leaflet.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,6 +6,14 @@ import "leaflet/dist/leaflet.css"
 
 const map = ref(null)
 
+function addNewFishery(){
+  console.log("Adding new Fishery")
+}
+
+function updateMapFishery(src){
+  console.log(src)
+}
+
 function formatMap(){
   console.log(map);
 
@@ -14,7 +22,7 @@ function formatMap(){
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(mapref);
+  }).addTo(mapref);
 
 }
 
@@ -28,11 +36,26 @@ onMounted(() =>{
 <template>
   <main>
 
+    <div class="row ms-3 mt-3">
+       <div class="col-4">
+          <select @change="updateMapFishery" class="form-select" aria-label="Default select example">
+            <option selected>Please select a fishery</option>
+            <option data-longitude="38.763250" data-latitude="-77.298793">Burke Lake</option>
+            <option data-longitude="38.742047" data-latitude="-77.387657">Bull Run</option>
+          </select>
+       </div> 
+       <div class="col-2">
+          <button class="btn btn-outline-primary" @click="addNewFishery">Add New Fishery</button>
+       </div>
+    </div>     
     
-
-    Hello
-
-    <div class="map-wrapper" ref="map"></div>
+    <div class="row">
+      <div class="col-4">
+        Side Menu
+      </div>
+      <div class="col-8 map-wrapper" ref="map"></div>
+    </div>  
+    
 
   </main>
 </template>
